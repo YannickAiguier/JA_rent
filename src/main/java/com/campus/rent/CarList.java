655d72d5fd59dbf46ad08788febd7707cae960de
YannickAiguier <yannick.aiguier@gmail.com>
2021-10-27 11:01:20 +0200

    Dev: ajout de la class CarList

---
+++ src/main/java/com/campus/rent/CarList.java
@@ -0,0 +1,25 @@
+package com.campus.rent;
+
+import java.util.List;
+
+public class CarList {
+	
+	private List<Car> carList;
+
+	/**
+	 * @return the carList
+	 */
+	public List<Car> getCarList() {
+		return carList;
+	}
+
+	/**
+	 * @param carList the carList to set
+	 */
+	public void setCarList(List<Car> carList) {
+		this.carList = carList;
+	}
+	
+	
+
+}
